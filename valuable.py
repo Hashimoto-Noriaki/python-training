27fea13d059267c2f561b201dd9120a9fbeaf98a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-12 22:35:09 +0900

    変数の練習

---
+++ valuable.py
@@ -0,0 +1,34 @@
+num = 100
+print(num)
+# 100
+
+# PEP8
+score = 90
+score01 = 80
+score_01 = 70
+
+print(score)
+print(score01)
+print(score_01)
+
+# 90
+# 80
+# 70
+
+# 間違った変数
+#point-01 = 10
+#01point
+#point$01
+
+#予約語は変数にできない
+# return = 1
+# print(return)
+
+
+# 予約後を調べる方法
+import keyword
+
+print(keyword.iskeyword('return'))
+print(keyword.iskeyword('Return'))
+#True
+#False
